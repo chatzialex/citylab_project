bee5c439c9b2e9f06f038c670e214624e081c28e
Alexandros Chatzikyriakos <chatzialex@yahoo.gr>
2023-12-17 19:48:56 +0000

    port sliding window min from patrol.cpp to patrol_with_service.cpp

--- robot_patrol/src/direction_service.cpp
+++ robot_patrol/src/direction_service.cpp
@@ -7,6 +7,7 @@
 #include <functional>
 #include <iterator>
 #include <memory>
+#include <numeric>
 
 using GetDirection = robot_patrol::srv::GetDirection;
 
@@ -28,84 +29,69 @@ private:
   constexpr static double kAngleFrontMin{kAngleRightMin + kPi / 3};
   constexpr static double kAngleLeftMin{kAngleFrontMin + kPi / 3};
   constexpr static double kAngleLeftMax{kPi / 2};
+  static constexpr size_t kSlidingSize{50};
 
   void service_cb(const std::shared_ptr<GetDirection::Request> request,
                   std::shared_ptr<GetDirection::Response> response);
 
   rclcpp::Service<GetDirection>::SharedPtr service_{};
+
+  std::vector<float> sliding_min_;
 };
 
 void DirectionService::service_cb(
     const std::shared_ptr<GetDirection::Request> request,
     std::shared_ptr<GetDirection::Response> response) {
-
-  const auto pos{[request](double angle) {
-    const auto index{
-        static_cast<size_t>(std::ceil((angle - request->laser_data.angle_min) /
-                                      request->laser_data.angle_increment))};
-    return request->laser_data.ranges.begin() + index;
+  const auto angleToIndex{[&request](float angle) {
+    return static_cast<size_t>(
+        std::ceil((angle - request->laser_data.angle_min) /
+                  request->laser_data.angle_increment));
   }};
-
-  const auto isWithinRange{[request](float range) {
+  const auto isWithinRange{[&request](float range) {
     return request->laser_data.range_min <= range &&
            range <= request->laser_data.range_max;
   }};
 
-  const auto range_aware_sum{[&isWithinRange](
-                                 std::vector<float>::iterator start,
-                                 std::vector<float>::iterator end) {
-    return std::accumulate(start, end, 0.0, [&isWithinRange](float a, float b) {
-      return isWithinRange(b) ? a + b : a;
-    });
-  }};
+  const auto i0{angleToIndex(kAngleRightMin)};
+  const auto i1{angleToIndex(kAngleFrontMin)};
+  const auto i2{angleToIndex(kAngleLeftMin)};
+  const auto i3{angleToIndex(kAngleLeftMax)};
 
-  const std::array<double, 3> sums{
-      range_aware_sum(pos(kAngleRightMin), pos(kAngleFrontMin)),
-      range_aware_sum(pos(kAngleFrontMin), pos(kAngleLeftMin)),
-      range_aware_sum(pos(kAngleLeftMin), pos(kAngleLeftMax))};
+  sliding_min_.resize(i3 - i0);
+  std::fill(sliding_min_.begin(), sliding_min_.end(), 0.0);
+  float min{};
 
-  const auto max_sum{std::max_element(sums.begin(), sums.end()) - sums.begin()};
-  /*
-  std::cout << "kAngleLeftMin:"
-            << pos(kAngleLeftMin) - request->laser_data.ranges.begin()
-            << std::endl;
-  std::cout << "kAngleFrontMin:"
-            << pos(kAngleFrontMin) - request->laser_data.ranges.begin()
-            << std::endl;
-  std::cout << "kAngleRightMin:"
-            << pos(kAngleRightMin) - request->laser_data.ranges.begin()
-            << std::endl;
-  std::cout << "kAngleRightMax:"
-            << pos(kAngleRightMax) - request->laser_data.ranges.begin()
-            << std::endl;
-
-  std::cout << "range_min:" << request->laser_data.range_min << std::endl;
-  std::cout << "range_max:" << request->laser_data.range_max << std::endl;
-
-  std::cout << "ranges:" << std::endl;
-
-  for (size_t i{0}; i < request->laser_data.ranges.size(); ++i) {
-    std::cout << i << ":" << request->laser_data.ranges[i] << " ";
+  for (size_t i{0}; i < sliding_min_.size(); ++i) {
+    min = 0;
+    for (size_t j{i0 + i - kSlidingSize}; j <= i0 + i + kSlidingSize; ++j) {
+      if (!isWithinRange(request->laser_data.ranges[j])) {
+        continue;
+      }
+      if (!min || (request->laser_data.ranges[j] < min)) {
+        min = request->laser_data.ranges[j];
+      }
+    }
+    sliding_min_[i] = min;
   }
 
-  std::cout << std::endl;
-  */
+  const std::array<double, 3> sums{
+      std::accumulate(sliding_min_.begin() /* + i0 - i0*/,
+                      sliding_min_.begin() + i1 - i0, 0.0),
+      std::accumulate(sliding_min_.begin() + i1 - i0,
+                      sliding_min_.begin() + i2 - i0, 0.0),
+      std::accumulate(sliding_min_.begin() + i2 - i0,
+                      sliding_min_.begin() + i3 - i0, 0.0)};
 
-  std::cout << "sums:" << sums[0] << " " << sums[1] << " " << sums[2]
-            << std::endl;
-  std::cout << "max_sum:" << max_sum << std::endl;
+  const auto max_sum{std::max_element(sums.begin(), sums.end()) - sums.begin()};
 
   switch (max_sum) {
   case 0:
-    std::cout << "right" << std::endl;
     response->direction = "right";
     break;
   case 1:
-    std::cout << "front" << std::endl;
     response->direction = "front";
     break;
   case 2:
-    std::cout << "left" << std::endl;
     response->direction = "left";
   }
 }
